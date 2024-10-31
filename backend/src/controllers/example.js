22a045c16200503d27f97d9121deee79f2633feb
hadijat <hadijatkareem10@gmail.com>
2024-10-31 12:21:43 +0000

    add score_endpoint

--- backend/src/controllers/example.js
+++ backend/src/controllers/example.js
@@ -1,4 +1,8 @@
 export const exampleAction = (req) => {
-    console.log(`Request: ${JSON.stringify(req)}`);
+    console.log(`Request: ${JSON.stringify(req.score)}`);
     return { message: 'Hello World'};
+}
+
+const func = (args) => {
+
 }
--- backend/src/router.js
+++ backend/src/router.js
@@ -1,8 +1,10 @@
 import RouterBuilder from "./router-builder.js";
 import { exampleAction } from "./controllers/example.js";
-
+import { Score_return } from "./controllers/score-endpoint.js";     
 const router = new RouterBuilder();
 
 router.addGetEndpoint('/example', exampleAction);
 
+
+router.addGetEndpoint("/return_score_getpoint",Score_return)
 export default router.router;
