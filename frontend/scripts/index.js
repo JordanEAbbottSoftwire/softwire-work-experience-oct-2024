87e1dd0cbc1429e60258a12426447e61370df724
Jordan Abbott <jordan.abbott@softwire.com>
2024-10-31 10:07:15 +0000

    Cleanup a bit and connect pages together

--- frontend/scripts/index.js
+++ frontend/scripts/index.js
@@ -1,1 +1,19 @@
-/* Leaving empty for now in place of routing in future */
+const addButtonListeners = () => {
+    const playButton = document.getElementById('play-button');
+    const tutorialButton = document.querySelector('#tutorial-button');
+    const hiscoresButton = document.querySelector('#hiscores-button');
+    
+    playButton.addEventListener('click', () => {
+        window.location.href = '/pages/game_page.html';
+    });
+    
+    tutorialButton.addEventListener('click', () => {
+        window.location.href = '/pages/how-to-play-page-tetris.html';
+    });
+    
+    hiscoresButton.addEventListener('click', () => {
+        window.location.href = '/pages/leaderboard.html';
+    });
+}
+
+window.onload = addButtonListeners;
