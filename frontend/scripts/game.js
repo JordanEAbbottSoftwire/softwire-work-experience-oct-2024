b430f5bfe14e63df778d34cfe2f9b22c90229695
Jordan Abbott <jordan.abbott@softwire.com>
2024-10-31 13:24:04 +0000

    Cleanup board draw

--- frontend/scripts/game.js
+++ frontend/scripts/game.js
@@ -3,23 +3,23 @@ import { createGame } from "./gameController.js";
 const gameController = createGame();
 const canvasElement = document.getElementById("gamegrid");
 const ctx = canvasElement.getContext('2d');
+const cellSize = 20;
 
-const gameBoard = gameController.getGameState().gameBoard;
+function drawBoard() {
 
-function drawBoard(gameBoard) {
     // clear the current canvas
     // loop through the gameboard and draw each cell
-   //
+    const gameBoard = gameController.getGameState().gameBoard;
     ctx.fillStyle = "white";
-    ctx.fillRect(0, 0, 400, 800);
+    ctx.fillRect(0,0, (gameBoard[0].length * cellSize) + 1, (gameBoard.length * cellSize) + 1);
     ctx.fillStyle = "black";
 
     for (let i = 0; i < gameBoard.length; i++) {
         const row =  gameBoard[i]
         for (let j = 0; j < row.length; j++){
-            ctx.fillRect(j * 20 + 1, i * 20 + 1, 19, 19);
+            ctx.fillRect(j * cellSize + 1, i * cellSize + 1, cellSize - 1, cellSize - 1);
         }
     }
 }
 
-drawBoard(gameBoard);
+drawBoard();
